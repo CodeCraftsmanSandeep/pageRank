ad0e6b66a4e7667ab5011ff37a77503bc568c22e
sandeep <161121811+CodeCraftsmanSandeep@users.noreply.github.com>
2024-11-24 17:28:06 +0530

    Update parallel_v4.cu

--- pageRankImplementations/parallel_v4.cu
+++ pageRankImplementations/parallel_v4.cu
@@ -4,7 +4,7 @@
 #include <sys/time.h>
 using namespace std;
 
-#define MAX_ITER 1000  // Maximum number of iterations
+#define MAX_ITER 1000 // Maximum number of iterations
 #define DAMPING_FACTOR 0.85
 #define THRESHOLD 1e-5
 
@@ -37,11 +37,13 @@ __global__ void finalPageRankKernel(const int* row_ptr, const int* col_idx, floa
 }
 
 __global__ void initializeContribution (float* contribution, const int* out_degree, const int num_nodes){
+    #define C 1
     const int u = blockIdx.x * blockDim.x + threadIdx.x;
-    if(u < num_nodes) contribution[u] = 1.0f / num_nodes / out_degree[u];
+    if(u < num_nodes) contribution[u] = C * 1.0f / num_nodes / out_degree[u];
+    #undef C
 }
 
-void pageRank(const int *row_ptr, const int *col_idx, const int* out_degree, int num_nodes, int num_edges) {
+float* pageRank(const int *row_ptr, const int *col_idx, const int* out_degree, int num_nodes, int num_edges) {
     int num_blocks = (num_nodes + 255) / 256;
     float random_contribution = (1.0f - DAMPING_FACTOR) / num_nodes;
     
@@ -53,8 +55,8 @@ void pageRank(const int *row_ptr, const int *col_idx, const int* out_degree, int
     int *d_out_degree;
     cudaMalloc(&d_out_degree, num_nodes * sizeof(int));
     cudaMemcpyAsync(d_out_degree, out_degree, num_nodes * sizeof(int), cudaMemcpyHostToDevice);
-    
-    // intializinf the contribution[u] = (1/n)/(out_degree[u])
+
+    // initialize page rank contribution values
     initializeContribution <<<num_blocks, 256>>> (d_old_contribution, d_out_degree, num_nodes);
 
     // allocating and copying CSR format to GPU
@@ -77,7 +79,7 @@ void pageRank(const int *row_ptr, const int *col_idx, const int* out_degree, int
         new_flag = !(new_flag);
     }
 
-    float rank[num_nodes];
+    float *rank = new float[num_nodes];
     if(new_flag){
         finalPageRankKernel <<< num_nodes, 256 >>> (d_row_ptr, d_col_idx, d_new_contribution, d_old_contribution, num_nodes);
         cudaMemcpy(rank, d_new_contribution, num_nodes * sizeof(float), cudaMemcpyDeviceToHost);
@@ -86,50 +88,101 @@ void pageRank(const int *row_ptr, const int *col_idx, const int* out_degree, int
         cudaMemcpy(rank, d_old_contribution, num_nodes * sizeof(float), cudaMemcpyDeviceToHost);
     }
 
-    printf("Final page rank values:\n");
-    for(int u = 0; u < num_nodes; u++) printf("pageRank[%d] = %f\n", u, rank[u]);
 
     cudaFree(d_row_ptr);
     cudaFree(d_col_idx);
     cudaFree(d_old_contribution);
     cudaFree(d_new_contribution);
     cudaFree(d_out_degree);
+    return rank;
 }
+
+struct Node{
+    int vertex;
+    struct Node* next;
+
+    Node(int vertex): vertex(vertex), next(nullptr) {} 
+};
 
 // assumptions:
 // 1) the graph is unweighted, directed.
 // 2) the graph may have multiple edges, self loops.
 
-int main() {
-    int num_nodes, num_edges;
-    scanf("%d %d", &num_nodes, &num_edges);
+float* computePageRank(const int num_nodes, const int num_edges, pair <int, int>* edges){
 
-    vector <vector <int>> in_neighbours(num_nodes);
+    Node** in_neighbours = new Node*[num_nodes];
+    Node** tail = new Node*[num_nodes];
+
+    for(int u = 0; u < num_nodes; u++){
+        tail[u] = in_neighbours[u] = nullptr;
+    }
+
     int* out_degree = (int*)calloc(num_nodes, sizeof(int));
 
     for(int edge = 0; edge < num_edges; edge++){
         int u, v;
-        scanf("%d %d", &u, &v);
-        in_neighbours[v].push_back(u);
+        u = edges[edge].first;
+        v = edges[edge].second;
+
+        if(tail[v] == nullptr){
+            tail[v] = in_neighbours[v] = new Node(u);
+        }else{
+            tail[v]->next = new Node(u);
+            tail[v] = tail[v]->next;
+        }
         out_degree[u]++;
     }
+    delete[] tail;
 
-    int in_neighbour_index[num_nodes + 1];  // Row array in CSR format
-    int in_neighbour[num_edges];            // Col array in CSR format
-
+    int* in_neighbour_index = new int[num_nodes + 1];  // Row array in CSR format
+    int* in_neighbour = new int[num_edges];            // Col array in CSR format
+    
     int edge = 0;
     in_neighbour_index[0] = 0;
     for(int v = 0; v < num_nodes; v++){
-        for(int& u: in_neighbours[v]) in_neighbour[edge++] = u;
-        in_neighbour_index[v+1] = in_neighbour_index[v] + in_neighbours[v].size();
+        int end = in_neighbour_index[v];
+        
+        Node* trav = in_neighbours[v];
+        while(trav != nullptr){
+            in_neighbour[edge++] = trav->vertex;
+            end++;
+            Node* next_ptr = trav->next;
+            delete trav;
+            trav = next_ptr;
+        }
+        in_neighbour_index[v+1] = end;
     }
+    delete[] in_neighbours;
 
-    double t1 = rtclock();
+
     // Call PageRank function
-    pageRank(in_neighbour_index, in_neighbour, out_degree, num_nodes, num_edges);
-    double t2 = rtclock();
+    float* rank = pageRank(in_neighbour_index, in_neighbour, out_degree, num_nodes, num_edges);
     
-    printf("Consumed time: %f\n", t2 - t1);
+   return rank;
+}
+
+void printPageRank(const int numNodes, const float* pageRank){
+    printf("\nFinal pageRank values:\n");
+    for(int u = 0; u < numNodes; u++) printf("pageRank[%d] = %.6f\n", u, pageRank[u]);
+    return;
+}
+
+int main() {
+    // Scanning |V|, |E|
+    int numNodes, numEdges;
+    scanf("%d %d", &numNodes, &numEdges);
+
+    // Storing all edges in heap memory
+    std::pair <int, int>* edges = (std::pair <int, int>*)malloc(numEdges * sizeof(std::pair <int, int>));
+    for(int i = 0; i < numEdges; i++) scanf("%d %d", &edges[i].first, &edges[i].second);
+
+    // Here currently the problem is only computing pageRank values, and not producing sorted order of nodes w.r.t pageRank values
+    double start_time = rtclock();
+    float* pageRank = computePageRank(numNodes, numEdges, edges);
+    double end_time = rtclock();
+
+    printPageRank(numNodes, pageRank);
+    printf("Time consumed: %f\n", end_time - start_time);
 
     return 0;
 }
